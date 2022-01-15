629f91f1c0ff56a6fe9bdb46c7a51970cd2d0a5e
steven <steven.h@live.com>
2022-01-15 15:02:48 -0800

    add header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+const Header = () => {
+    return (
+        <>
+            <h1>Memory card game (placeholder)</h1>
+            <div>Score: 0</div>
+            <div>Best score: 0</div>
+        </>
+    );
+};
+export default Header;
