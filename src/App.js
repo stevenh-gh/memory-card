9f3a9b500466178c5216c3206b9203fe9ec9009d
steven <steven.h@live.com>
2022-02-09 13:20:06 -0800

    render card in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Card from "./Card/Card";
 import Header from "./Header/Header";
 
 function App() {
     return (
         <div className="container m-auto">
             <Header />
-            {/*  */}
+            <Card />
         </div>
     );
 }
