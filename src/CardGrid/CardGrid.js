a6a2bd0fb5200a2f647bd82ce2b6c8756585fc67
steven <steven.h@live.com>
2022-02-10 13:44:42 -0800

    replace urls with array at an index

--- src/CardGrid/CardGrid.js
+++ src/CardGrid/CardGrid.js
@@ -44,41 +44,32 @@ const CardGrid = () => {
         return <Card char={char} />;
     };
     return (
-        <>
+        <div className="text-center">
             <div className="card-row">
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/d/d3/Character_Eula_Thumb.png"
+                {renderCard(charList[0]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/0/0a/Character_Ganyu_Thumb.png"
+                {renderCard(charList[1]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/a/a4/Character_Hu_Tao_Thumb.png"
+                {renderCard(charList[2]
                 )}
             </div>
             <div className="card-row">
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/f/f0/Character_Kaedehara_Kazuha_Thumb.png"
+                {renderCard(charList[3]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/b/b9/Character_Xiao_Thumb.png"
+                {renderCard(charList[4]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/1/1f/Character_Yanfei_Thumb.png"
+                {renderCard(charList[5]
                 )}
             </div>
             <div className="card-row">
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/c/cb/Character_Yun_Jin_Thumb.png"
+                {renderCard(charList[6]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/0/02/Character_Diluc_Thumb.png"
+                {renderCard(charList[7]
                 )}
-                {renderCard(
-                    "https://static.wikia.nocookie.net/gensin-impact/images/c/c2/Character_Zhongli_Thumb.png/"
+                {renderCard(charList[8]
                 )}
             </div>
-        </>
+        </div>
     );
 };
 export default CardGrid;
