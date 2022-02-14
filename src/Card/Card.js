f9c54159513aedb10b3ef60dc8da0f057d8d908f
steven <steven.h@live.com>
2022-02-14 13:14:10 -0800

    add borders and margins to cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,7 +1,10 @@
 const Card = (props) => {
     const { char, onClick } = props;
     return (
-        <div className="inline-block" onClick={onClick}>
+        <div
+            className="inline-block border-2 border-black rounded m-1"
+            onClick={onClick}
+        >
             <img src={char.url} />
             <div>{char.name}</div>
         </div>
