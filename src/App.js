453ea337872a4aaa9f5818689e48c4fedc1f130d
steven <steven.h@live.com>
2022-02-14 13:13:53 -0800

    update document title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 import CardGrid from "./CardGrid/CardGrid";
 import Header from "./Header/Header";
 
 function App() {
+    useEffect(() => {
+        document.title = "Genshin Memory Game";
+    }, []);
+
     return (
         <div className="container m-auto">
             <Header />
